b7b047b83f82fd0edb9af86d896d33c1a5392186
MkMuhammetKaradag <mk.muhammetkaradag@gmail.com>
2022-12-30 13:59:02 +0300

    added like actions

--- src/api/app/appApi.ts
+++ src/api/app/appApi.ts
@@ -3,15 +3,30 @@ import axios from "axios";
 import { getMyFollowUpsPostsRequestType } from "./appApiTypes";
 export const BASE_URL = LOCAL_URL;
 
-
 export const getMyFollowUpsPosts = async (
-    pageNuber: number = 0
-  ): Promise<getMyFollowUpsPostsRequestType> => {
-    const { data } = await axios.get(
-      `${BASE_URL}/Post/myFollowUpsPosts?pageNuber=${pageNuber}`,
-      {
-        withCredentials: true,
-      }
-    );
-    return data;
-  };
+  pageNuber: number = 0
+): Promise<getMyFollowUpsPostsRequestType> => {
+  const { data } = await axios.get(
+    `${BASE_URL}/Post/myFollowUpsPosts?pageNuber=${pageNuber}`,
+    {
+      withCredentials: true,
+    }
+  );
+  return data;
+};
+export const addPostLike = async (postId: string) => {
+  const { data } = await axios.put(
+    `${BASE_URL}/Post/like/${postId}`,
+    undefined,
+    {
+      withCredentials: true,
+    }
+  );
+  return data;
+};
+export const removePostLike = async (postId: string) => {
+  const { data } = await axios.delete(`${BASE_URL}/Post/like/${postId}`, {
+    withCredentials: true,
+  });
+  return data;
+};
